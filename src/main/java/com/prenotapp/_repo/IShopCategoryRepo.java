074a58874c91b33124e78ebe231497ba00461fa5
markdeavey <marcosdelbasso@hotmail.com>
2024-03-03 18:20:01 +0100

    Deleted unnecesary queries

--- src/main/java/com/prenotapp/_repo/IShopCategoryRepo.java
+++ src/main/java/com/prenotapp/_repo/IShopCategoryRepo.java
@@ -1,39 +1,6 @@
 package com.prenotapp._repo;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
-
-import com.prenotapp._dto.ShopCategoryDTO;
 import com.prenotapp._model.ShopCategory;
 
-public interface IShopCategoryRepo extends IGenericRepo<ShopCategory, Integer> {
-  @Query(
-    value = "DELETE FROM shop_category WHERE shop_id = ?1 AND category_id = ?2",
-    nativeQuery = true
-  )
-  void deleteCategoryFromShop(Integer shopId, Integer categoryId)
-    throws Exception;
-
-  @Query(
-    value = "SELECT * FROM shop_category WHERE shop_id = ?1 AND category_id = ?2",
-    nativeQuery = true
-  )
-  List<ShopCategoryDTO> listCategoriesByShop(
-    Integer shopId,
-    Integer categoryId
-  ) throws Exception;
-
-  @Query(
-    value = "SELECT * FROM shop_category WHERE shop_id = ?1 AND category_id = ?2",
-    nativeQuery = true
-  )
-  ShopCategory findByShopIdAndCategoryId(Integer shopId, Integer categoryId);
-
-  
-  @Query(
-    value = "SELECT * FROM shop_category WHERE shop_id = ?1",
-    nativeQuery = true
-  )
-  List<ShopCategory> findByShop(Integer shopid) throws Exception;
-}
+public interface IShopCategoryRepo
+  extends IGenericRepo<ShopCategory, Integer> {}
--- src/main/java/com/prenotapp/_repo/IShopRepo.java
+++ src/main/java/com/prenotapp/_repo/IShopRepo.java
@@ -1,32 +1,8 @@
 package com.prenotapp._repo;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.prenotapp._dto.CategoryDTO;
 import com.prenotapp._model.Shop;
 
 @Repository
-public interface IShopRepo extends IGenericRepo<Shop, Integer> {
-  @Query(
-    value = "INSERT INTO shop_category (id_shop, id_category) VALUES (?1, ?2)",
-    nativeQuery = true
-  )
-  void addCategorytoShop(Integer idShop, Integer idCategory);
-
-  @Query(
-    value = "DELETE FROM shop_category WHERE id_shop = ?1 AND id_category = ?2",
-    nativeQuery = true
-  )
-  void removeCategoryfromShop(Integer idShop, Integer idCategory);
-
-
-  
-  @Query(
-    value = "SELECT * FROM shop_category WHERE shop_id= ?1 AND category_id = ?2",
-    nativeQuery = true
-  )
-  List<CategoryDTO> listCategoriesByShopId(Integer shopId, Integer categoryId) throws Exception;
-}
+public interface IShopRepo extends IGenericRepo<Shop, Integer> {}
